c3473e650d64eb6c43aa1447ca00e50a82144ed3
muhammedr7025 <muhammedr7025@gmail.com>
2024-05-23 19:57:01 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/muapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.muapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:muapp/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Mu App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen());
+  }
+}
