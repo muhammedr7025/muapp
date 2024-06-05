49ca6ea57fede17be708db6dd48221b76d5d01f6
muhammedr7025 <muhammedr7025@gmail.com>
2024-06-05 11:21:33 +0530

    done

--- lib/core/notifier/ig_list_notifier.dart
+++ lib/core/notifier/ig_list_notifier.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-
 import '../../main.dart';
 import '../../model/lc_model.dart';
 import '../api/ig_list_api.dart';
 
-
-
 class ICNotifier extends ChangeNotifier {
-
   final IGListApi _igAPI = IGListApi();
   bool _isLoading = false;
 
@@ -15,25 +11,23 @@ class ICNotifier extends ChangeNotifier {
   ICModel? _iCModel;
   ICModel? get getIcModel => _iCModel;
 
-
-
-  Future<void> _igList({
-    required BuildContext context,
-  required String ig,required String district
-  }) async {
+  Future<void> _igList(
+      {required BuildContext context,
+      required String ig,
+      required String district}) async {
     try {
       _isLoading = true;
       notifyListeners();
       final listData = await _igAPI.iGListApi(ig: ig, district: district);
-      if(listData['hasError'] == false){
+      if (listData['hasError'] == false) {
         _iCModel = ICModel.fromJson(listData);
         _isLoading = false;
         notifyListeners();
       }
       _isLoading = false;
       notifyListeners();
-    } catch(error){
-       SnackBar snackBar = SnackBar(
+    } catch (error) {
+      SnackBar snackBar = SnackBar(
           backgroundColor: Colors.red, content: Text(error.toString()));
       snackbarKey.currentState?.showSnackBar(snackBar);
       _isLoading = false;
